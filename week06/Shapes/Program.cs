0797101ae67c52547302c547789292722068f00c
frdeleon-dev <chisco87@gmail.com>
2026-08-03 23:02:08 -0600

    Week 06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 5.0);
+        Console.WriteLine($"Square Color: {square.GetColor()}");
+        Console.WriteLine($"Square Area: {square.GetArea()}");
+
+        Circle circle = new Circle("Blue", 3.0);
+        Console.WriteLine($"Circle Color: {circle.GetColor()}");
+        Console.WriteLine($"Circle Area: {circle.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Green", 4.5, 6.0);
+        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}");
+        Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape} Color: {shape.GetColor()}");
+            Console.WriteLine($"{shape} Area: {shape.GetArea()}");
+        }
     }
 }
